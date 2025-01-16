6fba8af9cccc573f6608798c9143229547398bea
Minjo Park <consistent.312@gmail.com>
2025-01-16 10:15:48 +0900

    반복문 2문제 해결

--- part1/07.반복문/(lv.1)sum-up-to.js
+++ part1/07.반복문/(lv.1)sum-up-to.js
@@ -11,16 +11,17 @@
  */
 function sumUpTo(n) {
   // TODO: n이 1 이상이면 1부터 n까지의 합을 반환, 0 이하이면 0을 반환
-  // if (n >= 1) {
-  //   let i = 1;
-  //   while (i < n) {
-  //     n += i;
-  //     i++;
-  //   }
-  //   return n;
-  // } else {
-  //   return 0;
-  // }
+  let sum = 0;
+  if (n >= 1) {
+    let i = 1;
+    while (i <= n) {
+      sum += i;
+      i++;
+    }
+    return sum;
+  } else {
+    return 0;
+  }
 }
 
 // export를 수정하지 마세요.
